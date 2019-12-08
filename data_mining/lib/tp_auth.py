936edb55e429fd3bf4555ef3b6dd4b3b3633618b
Valentina Roquemen <valentina.roquemen@udea.edu.co>
2019-12-08 11:05:20 -0500

    Libraries for data mining

---
+++ data_mining/lib/tp_auth.py
@@ -0,0 +1,56 @@
+import tweepy
+import json
+        
+def api_auth(usr_id):
+    ''' Function to authenticate id. Reads credentials from json file. '''
+
+    try:
+        with open("utils/keysAndTokens.json","r") as rfile:
+        
+            data = json.load(rfile)
+            
+            usr_found = False
+            for usr in data:
+                if usr["user_id"] == usr_id:
+                    auth = tweepy.OAuthHandler(usr["API_key"], usr["API_secret_key"])
+                    auth.set_access_token(usr["acces_token"], usr["acces_token_secret"])
+                
+                    api = tweepy.API(auth)
+                        
+                    usr_found = True
+            
+                    return api
+              
+            if not usr_found:
+                raise ValueError("User ID not found.")
+
+    except FileNotFoundError:
+         with open("utils/keysAndTokens.json","r") as rfile:
+        
+            data = json.load(rfile)
+            
+            usr_found = False
+            for usr in data:
+                if usr["user_id"] == usr_id:
+                    auth = tweepy.OAuthHandler(usr["API_key"], usr["API_secret_key"])
+                    auth.set_access_token(usr["acces_token"], usr["acces_token_secret"])
+                
+                    api = tweepy.API(auth)
+
+                    usr_found = True 
+
+                    return api
+            if not usr_found:
+                raise ValueError("User ID not found.")
+
+
+def get_credentials(usr_id):
+    ''' Function to get dict with credentials from specified user id on json file '''
+    with open("keysAndTokens.json","r") as rfile:
+        
+        data = json.load(rfile)
+
+        for usr in data:
+            if usr["user_id"] == usr_id:
+
+                return usr
