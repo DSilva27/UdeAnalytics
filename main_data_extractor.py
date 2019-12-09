9de1de2c52133dec9c9d004f4a80f48d29c59fcc
vlt-ro <valentina.roquemen@udea.edu.co>
2019-12-08 23:25:03 -0500

    main with stats

--- main_data_extractor.py
+++ main_data_extractor.py
@@ -4,6 +4,7 @@
 #---------------------------------------------------------------
 
 # Libraries
+import sys
 import pandas as pd
 import numpy as np
 import json
@@ -34,8 +35,8 @@ if __name__ == "__main__":
     file_followers = "data/data_followers.json"
 
     infop = ss.add_follow_list(users,file_following,file_followers)
+    n_users = len(infop)
     infop = infop[infop.Following!=0] 
-    #print(infop.index)
 
     # Metric
 
@@ -68,3 +69,49 @@ if __name__ == "__main__":
     NodeWeight = ss.node_weight(usr_id_scrn,counter)
 
     np.savetxt('data/NodeW.csv',NodeWeight)
+
+
+    clusters = pd.read_csv("data/node_segmentation.csv",sep=';' )
+    groups = clusters.groupby('NODE_NUM')     
+
+    f = open('data/estadistica.txt', 'w')
+
+    print("----------------Estadistica general-------------",file=f)
+    print("Numero de usuarios:",n_users,file=f)
+    print("Numero de usuarios con cuentas no privadas:",len(infop),file=f)
+    print("Numero de parejas de usuarios que se siguen entre si:",int(len(FF[FF==2])/2),file=f)
+    print("Numero maximo de RT entre usuarios:",int(np.max(RT)),file=f)
+    print("Numero maximo de Rep de un usuario a otro:",int(np.max(REP)),file=f)
+    print("Promedio de RT entre usuarios:",int(np.mean(RT)),file=f)
+    print("Promedio de Rep entre usuarios:",int(np.mean(REP)),file=f)
+    print("Numero de clusters:",len(groups),file=f)
+    print("Numero de clusters con mas de una persona:",len(groups.count().NODE_RANK[groups.count().NODE_RANK>1]),file=f)
+
+
+    for group in groups:
+        if len(group[1])>1: 
+            usr_id_scrn['bool'] = np.zeros(len(usr_id_scrn))
+
+            for user in group[1]["SCREEN_NAME"]:
+                 usr_id_scrn.loc[usr_id_scrn[0]==user,'bool'] = 1
+
+            user_id = usr_id_scrn.groupby('bool').get_group(1.0)
+            info_cluster = infop.loc[group[1]["SCREEN_NAME"]]
+
+            
+            counter_cluster = ss.RT_REP_Counter(data, user_id)
+
+            RT = ss.interaction('RT',user_id,counter_cluster)
+            REP = ss.interaction('Rep',user_id,counter_cluster)
+
+            FF = ss.follower_following_count(info_cluster)
+            
+            print("----------------Estadistica Cluster %i-------------"%group[0],file=f)
+            print("Numero de usuarios:",len(group[1]),file=f)
+            print("Numero de parejas de usuarios que se siguen entre si:",int(len(FF[FF==2])/2),file=f)
+            print("Numero maximo de RT entre usuarios:",int(np.max(RT)),file=f)
+            print("Numero maximo de Rep de un usuario a otro:",int(np.max(REP)),file=f)
+            print("Promedio de RT entre usuarios:",int(np.mean(RT)),file=f)
+            print("Promedio de Rep entre usuarios:",int(np.mean(REP)),file=f)
+
+f.close()
